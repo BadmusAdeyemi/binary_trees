b817f29e0e4e151c22975e4993dfd76c6468abb7
BadmusAdeyemi <111635418+BadmusAdeyemi@users.noreply.github.com>
2023-03-01 21:52:06 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -28,6 +28,18 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/**
+ * struct line - struct for tree list
+ * 
+ * @node: tree
+ * @next: node next
+*/
+typedef struct line
+{
+    binary_tree_t *node;
+    struct line *next;
+} line_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
